32d94b46e3c1810307fcf0974cfb93cda0063a4a
James Turnham <james.turnham@gmail.com>
2018-08-12 09:49:42 -0400

    Temp fix for logger issue

--- scrycord.js
+++ scrycord.js
@@ -3,7 +3,7 @@ var logger = require('winston');
 var auth = require('./auth.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
+logger.add(new logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
@@ -13,9 +13,9 @@ var bot = new Discord.Client({
    autorun: true
 });
 bot.on('ready', function (evt) {
-    // logger.info('Connected');
-    // logger.info('Logged in as: ');
-    // logger.info(bot.username + ' - (' + bot.id + ')');
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Listen for commands starting with '!'
